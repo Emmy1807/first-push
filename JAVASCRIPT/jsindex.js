500c73a5fd9f1028b6cbfd1c36aa579d6984a7bd
Emmanuel <emmanuelonuh141@gmail.com>
2024-11-16 13:54:25 +0100

    second commit

---
+++ JAVASCRIPT/jsindex.js
@@ -0,0 +1,559 @@
+// console.log(34343434)
+// //alert(73948984949488)
+// //prompt(494840409)
+
+// console.log("My name is Emmy")
+// console.log("my pets name")
+// console.log(58+8)
+// console.log(true)
+// console.log(null)
+
+// var myName = "var: My name is Emmy and i will be d best in coding "
+// console.log(myName)
+
+// let Greeting = "let: Hello World"
+// console.log(Greeting)
+
+// let Greet="const: Good afternoon"
+// console.log(Greet)
+
+// //let name = prompt ("what is your name")
+// console.log("Hello" + name)
+
+// let firstname = "Emmanuel"
+// let  lastname = "Onuh"
+// let num = 49332
+// let num2 =44334323
+// console.log (firstname + lastname, num, num2)
+// //const empty = ""
+
+// console.log(lastname. indexOf("n"))//used the alphabet to find the index.
+// console.log(lastname[3])//used the index to find the alphabet.
+
+// console.log(firstname.concat(lastname))
+
+// //string lenth for getting the length of the string
+// console.log(firstname.length)
+
+// //string length and slicing
+
+// //string slice for cutting out some part of the string to form a new string
+// console.log(firstname.slice(0,5))
+
+// let longest ="pneumonoultramicroscopicsilicovolcanoconiosis"
+// console.log(longest[9] + longest[2] + longest[12] + longest[16] + longest[1])
+
+// let word ="pneumonoultramicroscopicsilicovolcanoconiosisd"
+// console.log(word[15] + word[17] + word[45] + word[2])
+
+
+// //string template literals
+// //A function in javascript used combining a string and a variable 
+// //symbolized using backtick,
+
+//const promm = prompt("what is your name")
+//console.log(`my name is ${promm}`)
+
+// //Modulus(%)
+// const nuuu = 1
+// console.log(nuuu)
+
+
+// //Increment(++)
+// let n = 4
+// n+=6
+// console.log(n)
+
+
+// //decrement(--)
+// let nn = 4
+// nn-=5
+// console.log(nn)
+
+
+// //Random Number and Math Objects
+// //How otp generate random numbers in javascript
+// console.log(Math.random())
+
+
+// Math.floor(); // converts it to the nearest lowest whole number 
+// Math.ceil(); // converts it to the nearest highest whole number 
+// console.log(Math.ceil(Math.random() *1000000))
+
+
+// //conditional Ternery 
+// //Decision making in Javascript(if, else, else if)
+
+
+// const condition = 10
+// if(condition >4) {
+//     console.log("E is not a number")
+// }
+// else if(condition > 4 && condition < 14){
+//     console.log("E is s number less than 14")
+// }
+// else if(condition > 14 && condition < 18){
+//     console.log("E is a number less than 18")
+// }
+// else{
+//     console.log("Do what ever you want to do")
+// }
+
+// const time = prompt("What is the time")
+// if(time >=1 && time <=12) {
+//     console.log("Good morning")
+// }
+// else if(time >=12 && time <=18) {
+//     console.log("Good evening")
+// }
+// else if(time >=18 && time <=24) {
+//     console.log("Good night")
+// }
+// else{
+//     console.log("This time doesn't exist")
+// }
+
+// // console.log(324245)
+
+// // const numbers = prompt("six digits number")
+// // if(numbers >=100000 && numbers <=999999){
+// //     console.log("Successful")
+// // }
+// // else if(numbers >=999999){
+// //     console.log("Sorry You Don't Have Access To This")
+// // }
+
+// const random = Math.ceil(Math.random()*1000000)
+// alert(random)
+
+// const numbers = Number(prompt("Input One-Time Code"))
+
+// if(random ===numbers){
+//     console.log("Successful")
+// }
+// else{
+//     console.log("Sorry You Don't Have Access To This ")
+// }
+
+// const Cars =[
+//     Car1 ={
+//         make:"Cyber Truck",
+//         Model:"2020 model",
+//         speed:"290km/h"
+//     },
+//     Car2 ={
+//         make:"Benz",
+//         Model:"2024 model",
+//         speed:"250km/h"
+//     },
+//     Car3 ={
+//         make:"Toyota",
+//         Model:"2022 model",
+//         speed:"260km/h"
+//     },
+//     Car4 ={
+//         make:"Bugatti",
+//         Model:"2023 model",
+//         speed:"240km/h"
+//     },
+// ]
+
+// console.log(Cars[0].speed)
+// console.log(Cars[1])
+// console.log(Cars[2])
+// console.log(Cars[3])
+
+
+// const chibyk = true
+// const martins = false
+// const ernest = true
+
+// if(chibyk && ernest && martins){
+//     console.log("All are true")
+// }
+// else{
+//     console.log("One is stubborn")
+// }
+
+
+
+// // Array - collection of multiple data into a single variable.
+// // The array is donated using square bracket []
+
+// const arr = ["philip", "solomon", "chidi",]
+//  //arr.pop()
+//  console.log(arr)
+
+// arr.push("emmy")
+// console.log(arr)
+// // console.log(arr)
+// // console.log(arr[2])
+// // arr[0] = "vivian"
+// // console.log(arr)
+// // arr[2] = "Emmy"
+// // arr[3] = "Iyke"
+// // console.log(arr)
+
+
+// console.log(Math.random()*10000)
+
+// console.log(Math.floor(Math.random()*9))
+// console.log(Math.ceil(Math.random()*9))
+
+// // javascript object. it is symbolized using the {}
+// const person = {
+//     name: "Emma",
+//     age: "20",
+//     address: "River state",
+//     adult:true 
+// }
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.address)
+
+// //while loop: mostly used when the variables are not known or unsure
+// let i = 2
+// while (i < 20){
+//     console.log(i);
+//     i++;
+// }
+
+// // Functions in Javascript
+// //function addNumber() {
+//    // console.log(3 + 5)
+// //}
+
+// //addNumber();
+
+// //const header = document.getElementById("hd")
+// //const button = document.getElementById("btn")
+
+// //console.log(header)
+// //console.log(button)
+
+// //button.addEventListener("click", function(){
+//   //  header.textContent++
+// //})
+
+// //btn2.addEventListener("click", function(){
+//   //  header.textContent--
+// //})
+
+// //function multiply(a, b){
+//   //  console.log(a + b)
+// //}
+// //multiply(2, 5)
+// //const hd = document.getElementById("hd")
+// //const hd2 = document.getElementById("hd2")
+// //const hd3 = document.getElementById("hd3")
+// //create a function that adds two numbers 
+// //hd3.textContent = 0
+// //function add(a, b) {
+//     //hd.textContent = a
+//     //hd2.textContent = b
+//     //hd3.textContent = a + b;
+// //}
+
+// //add(9, 5)
+
+
+const endgame = document.getElementById("endGame")
+const endgame2 = document.getElementById("endGame2")
+const endgame3 = document.getElementById("endGame3")
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+
+function addNumbers(a,b){
+    endgame.textContent = a
+    endgame2.textContent = b
+    endgame3.textContent = a + b
+    console.log(a, b)
+}
+
+function subtractNumbers(a,b){
+    endgame.textContent = a
+    endgame2.textContent = b
+    endgame3.textContent = a - b
+    console.log(a, b)
+} 
+
+btn1.addEventListener("click", function(){
+    addNumbers(2, 4)
+})
+
+btn2.addEventListener("click", function(){
+    subtractNumbers(1, 3)
+})
+
+function createNumber(ca) {
+    console.log("Hello")
+    return ca();
+}
+
+function anotherFunction () {
+    console.log("call back")
+}
+
+createNumber(anotherFunction)
+
+// Variable scope in javascript
+const variablee = "hello"
+
+function greetName() {
+    let time = 12
+    if(time<12){
+        var timee2 = 23
+    }else{
+        console.log(timee2 + 1)
+        console.log("No greet")
+    }
+    console.log(timee2)
+
+   // console.log(time)
+}
+
+greetName();
+
+// Arrow function
+const fnxName = () => console.log("Arrow function") 
+
+
+const Objects = [
+    {
+        Name:"My Name is Emmanuel",
+        Age:"20",
+        Height:"6.5",
+        Desc:"i'm dark in complexion, good looking & D-best"
+    },
+    {
+        Name:"David",
+        Age:"22",
+        Height:"6.8",
+        Desc:"Loves gaming"
+    },  
+    {
+        Name:"Vincent",
+        Age:"32",
+        Height:"6.9",
+        Desc:"very tall, ambitious and serious minded"  
+    }, 
+    {
+        Name:"Chibyke",
+        Age:"20",
+        Height:"6.5",
+        Desc:"simple lifestyle"
+    },
+    {
+        Name:"Gift",
+        Age:"20",
+        Height:"6.5",
+        Desc:"very beautiful, loves watching movies "
+    },
+    {
+        Name:"Prince",
+        Age:"20",
+        Height:"6.5",
+        Desc:"Strict and very business minded"
+    }               
+]
+let input = prompt("User")
+if(input ==0||input==Objects[0].Name){
+    console.log(`${Objects[0].Name} ${Objects[0].Desc}`)
+    endgame.textContent = `${Objects[0].Name} ${Objects[0].Desc}`
+}
+if(input ==0||input==Objects[1].Name){
+    console.log(`${Objects[1].Name} ${Objects[1].Desc}`)
+    endgame2.textContent = `${Objects[1].Name} ${Objects[1].Desc}`
+}
+if(input ==0||input==Objects[2].Name){
+    console.log(`${Objects[2].Name} ${Objects[2].Desc}`)
+    endgame3.textContent = `${Objects[2].Name} ${Objects[2].Desc}`
+}
+if(input ==0||input==Objects[3].Name){
+    console.log(`${Objects[3].Name} ${Objects[3].Desc}`)
+    btn1.textContent = `${Objects[3].Name} ${Objects[3].Desc}`
+}
+if(input ==0||input==Objects[4].Name){
+    console.log(`${Objects[4].Name} ${Objects[4].Desc}`)
+    btn2.textContent = `${Objects[4].Name} ${Objects[4].Desc}`
+}
+
+
+    //console.log(Objects[0])
+
+// Recursive functions in javascript: a funtion that calls itself
+
+//function classs(a){
+
+    // Base condition/ state
+  //  if(a <= 0){
+   //     console.log("Done")
+     //   return;
+    //}
+    // Recursive condition/state
+    //console.log(a)
+    //classs(a - 1)
+//}
+//classs(7);
+
+// using recursive function to calculate the sum of an array
+const someArray = [2, 4, 5, 6, 9]
+
+function sumOfArray(n){
+    if(n.lenght === 0){
+        console.log("Hurray!!")
+        return 0;   
+    }
+
+    console.log(n)
+    return n[0] + sumOfArray(n.slice(1))
+}
+//console.log(sumOfArray(someArray))
+
+const fruits = ["mango", "orange", "pineapple", "indomie", "watermelon"]
+
+fruits.forEach(function(elem, dex, ray){
+    console.log(`${elem} is at the index of ${dex} inside the ${ray} array`)    
+    
+})
+
+const products = [
+    { name: "Laptop", price: 1000},
+    { name: "Phone", price: 600},
+    { name: "Tablet", price: 400},
+];
+
+// laptop price is 1000 but the discounted price is 1000 * 0.5
+
+products.forEach(function(eachItem){
+    eachItem.disCountedPrice = eachItem.price * 0.5
+    console.log(`The price of ${eachItem.name} is ${eachItem.price}
+    but the discounted price is ${eachItem.disCountedPrice}`)
+})
+
+const birds = [
+    { size: "big size", price: 27000},
+    { size: "medium size", price: 25000},
+    { size: "small size", price: 22000},
+];
+
+birds.forEach(function(eachbirds){
+    eachbirds.disCountedPrice = eachbirds.price * 0.9
+    console.log(`The price of ${eachbirds.size} is ${eachbirds.price}
+    but the discounted price is ${eachbirds.disCountedPrice}`)
+})
+
+// mmap function
+
+// map method: a javascript function that iterates over an array  and execute a 
+//provided fuction on each of the element. It doesn't modify the array
+
+
+fruits.map(function(element, dex, ray){
+    console.log(element + 1)
+})
+
+const people = [
+    {name: "Emma", desc: "He is good looking and smart"},
+    {name: "David", desc: "He is hardworking"},
+    {name: "Favour", desc: "He is a nice and free"},
+    {name: "Gift", desc: "He is so caring"},
+    {name: "Sam", desc: "He is serious minded"},
+    {name: "Tega", desc: "He is determined"},
+];
+//people.map((element, index, array) => {
+  //  console.log(element.name)
+//})
+
+// find function
+// an array function that checks for first element that satisfies a conditon
+//const num = [ 2, 6, 5, 3, 7, 10]
+
+//const fin = num. find(function(element, index, array){
+  //  return element > 5
+//})
+//console.log(fin)
+
+// filter function
+// an array function that checks for first element that satisfies a condition
+//const numb = [ 2, 6, 5, 3, 7, 10]
+
+//const fine = num. filter(function(element, index, array){
+  //  return element > 5
+//})
+//console.log(fine)
+
+const ask = prompt("Search for name"). trim();
+
+const result = people.find((elem) => ask === elem.name);
+    if (result) {
+        console.log(result.desc);
+    } else {
+        console.log("No match found")
+    }
+    //if(ask === elem.name){
+        //return elem.desc;
+    //}
+//})
+//console.log(fine)
+    
+
+// spread in objects, array literal and function calls
+const nom = [ 2, 6, 5, 3, 7, 10]
+const nom2 = [ 4, 5, 12, 45]
+console.log(...nom, ...nom2)
+
+const vehicles = ['mustang', 'f-150', 'BMW' ]
+// Destructuring
+const [ aa, bb, cc] = vehicles
+console.log(aa)
+console.log(cc)
+
+// objects destructuring
+const pep = {
+    name: "Emma",
+    desc: "He is okay"
+}
+const { name, desc } = pep
+console.log(name)
+console.log(desc)
+
+const user = {
+    id: 1,
+    profile: {
+        username: "Johndoe",
+        country: "New York"
+    }
+};
+
+const { id, profile: { country,username} } = user;
+console.log(id);
+console.log(username);
+console.log(country);
+
+// without destructuring
+function greet(user){
+    console.log(`Hello ${user.name} your age is ${user.age}`)
+}
+const use = greet({ name: "Vivian", age: 20 })
+console.log(use)
+
+// with destructuring
+function greet2({ name, age }){
+    return `Hello ${name} your age is ${age}`
+}
+
+
+const use2 = greet2({ name: "Vicky", age: 30})
+console.log(use2)
+
+// Reduce function: a javascript method that iterates through each element in an array
+//and applies a callback function, accumulating a single output, mostly used for summing 
+// array, flattening array etc.
+
+const red = [ 2, 4, 6, 7]
+
+const red2 = red.reduce(function( acc, currenValue, index, array){
+    return acc + currenValue
+})
+console.log(red2)
